1744cd812fed0720a5d7a6a60b9cb3e27b3675bb
DarkMetalMouse <ilaisgv@gmail.com>
2021-02-27 22:21:42 +0200

    added buttons to arduino file

--- arduino/deej-5-sliders-vanilla/deej-5-sliders-vanilla.ino
+++ arduino/deej-5-sliders-vanilla/deej-5-sliders-vanilla.ino
@@ -1,5 +1,15 @@
+// made for momentary (non latching) buttons
+// debounce provided
+
+#define DEBOUNCE 200
+
 const int NUM_SLIDERS = 5;
-const int analogInputs[NUM_SLIDERS] = {A0, A1, A2, A3, A4};
+const int analogInputs[NUM_SLIDERS] =  {A0, A1, A2, A3, A4};
+const int digitalInputs[NUM_SLIDERS] = {2,  3,  4,  5,  6};
+
+int digitalInputsStates[NUM_SLIDERS] = {HIGH};
+int digitalInputsPrevStates[NUM_SLIDERS] = {HIGH};
+long digitalInputsTimes[NUM_SLIDERS] = {0};
 
 int analogSliderValues[NUM_SLIDERS];
 
@@ -13,6 +23,7 @@ void setup() {
 
 void loop() {
   updateSliderValues();
+  updateButtonStates();
   sendSliderValues(); // Actually send data (all the time)
   // printSliderValues(); // For debug
   delay(10);
@@ -24,12 +35,29 @@ void updateSliderValues() {
   }
 }
 
+void updateButtonStates() {
+  for (int i = 0; i < NUM_SLIDERS; i++) {
+    int reading = digitalRead(digitalInputs[i]);
+    
+    if (reading == HIGH && digitalInputsPrevStates[i] == LOW && millis() - digitalInputsTimes[i] > DEBOUNCE) {
+      digitalInputsStates[i] ^= 1; // invert value
+      
+      digitalInputsTimes[i] = millis();    
+    }
+  
+    digitalInputsPrevStates[i] = reading;
+  }
+}
+
+
 void sendSliderValues() {
   String builtString = String("");
 
   for (int i = 0; i < NUM_SLIDERS; i++) {
     builtString += String((int)analogSliderValues[i]);
-
+    builtString += ",";
+    builtString += String((int)digitalInputsStates[i]);
+    
     if (i < NUM_SLIDERS - 1) {
       builtString += String("|");
     }
